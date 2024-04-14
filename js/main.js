fc29e8d23364d755426a2b2204312f185cedd96d
HuslT1me <152012295+HuslT1me@users.noreply.github.com>
2024-04-14 15:22:01 +0300

    added links and change color of them

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-const burgerBtn = document.querySelector(".header__burger-btn");
-const openMenu = document.querySelector(".mobile-menu");
+const burgerBtn = document.querySelector(".header__burger-btn"),
+  openMenu = document.querySelector(".mobile-menu"),
+  openCategoriesBtn = document.querySelector(".nav-link--categories"),
+  categoriesPopup = document.querySelector(".open-categories-popup");
 
 burgerBtn.addEventListener("click", () => {
   openMenu.classList.toggle("is-open");
   burgerBtn.classList.toggle("is-active");
 
-  if (document.body.style.overflow === 'hidden') {
-        document.body.style.overflow = null;
-    } else {
-        document.body.style.overflow = 'hidden';
-    }
+  if (document.body.style.overflow === "hidden") {
+    document.body.style.overflow = null;
+  } else {
+    document.body.style.overflow = "hidden";
+  }
 });
+
+openCategoriesBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // categoriesPopup.classList.toggle("is-open");
+
+  // window.addEventListener("click", (clickOnBody) => {
+  //     if (clickOnBody.target === categoriesPopup) {
+  //       categoriesPopup.classList.remove("is-open");
+  //     }
+  // })
+});
+
