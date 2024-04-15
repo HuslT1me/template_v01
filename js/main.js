bdf380eaa90a1ae3001f1a3ff76f7546df5c9aa2
HuslT1me <152012295+HuslT1me@users.noreply.github.com>
2024-04-15 11:28:42 +0300

    added tabs in video page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ const burgerBtn = document.querySelector(".header__burger-btn"),
   openMenu = document.querySelector(".mobile-menu"),
   openCategoriesBtn = document.querySelector(".nav-link--categories"),
   categoriesPopup = document.querySelector(".open-categories-popup"),
-  tabsVideo = document.querySelectorAll(".categories__tabs-item");
+  tabsVideo = document.querySelectorAll(".categories__tabs-item"),
+  tabsContent = document.querySelectorAll(".categories__thumbnail");
 
 burgerBtn.addEventListener("click", () => {
   openMenu.classList.toggle("is-open");
@@ -27,10 +28,30 @@ openCategoriesBtn.addEventListener("click", (e) => {
   // })
 });
 
-for (let i = 0; i < tabsVideo.length; i++) {
-  tabsVideo[i].addEventListener("click", (btns) => {
-    btns.preventDefault();
+const onTabClick = () => {
+  for (let i = 0; i < tabsVideo.length; i++) {
+    tabsVideo[i].addEventListener("click", (btns) => {
+      btns.preventDefault();
 
-    tabsVideo[i].classList.add("tab--active");
-  });
-}
+      let tabId = tabsVideo[i].getAttribute("href");
+      let currentTab = document.querySelector(tabId);
+
+      if (!tabsVideo[i].classList.contains("tab--active")) {
+        tabsVideo.forEach((item) => {
+          item.classList.remove("tab--active");
+        });
+
+        tabsContent.forEach((tab) => {
+          tab.classList.remove("content--active");
+        });
+
+        tabsVideo[i].classList.add("tab--active");
+        currentTab.classList.add("content--active");
+      }
+    });
+  }
+};
+
+onTabClick();
+
+document.querySelector(".categories__tabs-item").click();
